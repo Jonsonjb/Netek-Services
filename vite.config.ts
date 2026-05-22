3f7ae9d041aa045e7002827b74c124f33f9bd10e
Jonsonjb <Johsondagloria@gmail.com>
2026-05-22 18:11:41 +0200

    git up

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Netek-Services/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
